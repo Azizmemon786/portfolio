c504cdac7d03a5f5d74bef5cd6c3d91b32d51470
Azizmemon786 <136925916+Azizmemon786@users.noreply.github.com>
2023-06-22 18:37:19 -0700

    Aziz

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x'); 
+    navbar.classList.toggle('active');
+};
+
+    let header = document.querySelector('header');
+     
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-caret-right');
+    navbar.classList.remove('active');
+
